4306ddfe651e6fbde13e5350af93890c61261bd0
Besgmo <Besgmo@gmail.com>
2021-03-14 20:20:06 +0200

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6049d8478db767001e87ce59",
+				"6049d8478db767001e87ce5b"
+			]
+		},
+		"6049d8478db767001e87ce59": {
+			"id": "6049d8478db767001e87ce59",
+			"name": "Settings",
+			"pageUrl": "404"
+		},
+		"6049d8478db767001e87ce5b": {
+			"id": "6049d8478db767001e87ce5b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
